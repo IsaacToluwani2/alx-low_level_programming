0179b52212d54d9c7d304e0bab117dacc26f316d
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-05-31 22:03:46 +0100

    betty style implemented and errors corrected

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
 
 /** 
- * main -
+ * main - Entry point of the program
  *
- * Return: 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,7 +17,7 @@ int main(void)
 	
     lDigit = abs(n) % 10;
 
-    printf(" Last digit of %d is", n);
+    printf(" Last digit of %d is %d ", n, lDigit);
     if (lDigit > 5)
     {
         printf("and is greater than 5\n");
@@ -29,7 +30,6 @@ int main(void)
     {
         printf("and is less than 6 and not 0\n");
     }
-    
     
 	return (0);
 }
