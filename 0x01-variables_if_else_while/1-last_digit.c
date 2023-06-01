d73a17fd4e42e23b225b69b686bea054ad1f2ac3
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-06-01 07:43:12 +0100

    betty coding style implemented

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ lDigit = (n) % 10;
 printf("Last digit of %d is %d ", n, lDigit);
 if (lDigit > 5)
 {
-    printf("and is greater than 5\n");
+printf("and is greater than 5\n");
 }
 if (lDigit == 0)
 {
-    printf("and is 0\n");
+printf("and is 0\n");
 }
 if ((lDigit < 6) && (lDigit != 0))
 {
-    printf("and is less than 6 and not 0\n");
+printf("and is less than 6 and not 0\n");
 }
 
 return (0);
