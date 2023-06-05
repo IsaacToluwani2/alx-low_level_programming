32715151c890ea07a0f8c34c434b0048ac3eb4ed
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-06-05 14:08:25 +0100

    new task completed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar(10);
+return (0);
+}
