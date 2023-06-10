8c9d269f1676d075e2077ca9f2b94604f0ac7ee1
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-06-10 13:46:20 +0100

    task done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - look for lower case
+ * @c: The character to print
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+switch (n)
+{
+case '(n > 0)':
+    return (1);
+    break;
+
+case '(n = 0)':
+    return (1);
+    break;
+
+case '(n < 0)':
+    return (-1);
+    break;
+
+default:
+    break;
+}
+}
