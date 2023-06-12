1276b53f42be607881d6ff855f008304223936a3
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-06-12 08:55:50 +0100

    error corrected

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,20 +11,20 @@ int print_sign(int n)
 if (n>0)
 {
 _putchar(43);
-putchar('\n');
+_putchar('\n');
 return (1);
 }
 if (n = 0)
 {
 _putchar(48);
-putchar('\n');
+_putchar('\n');
 return (0);
 
 }
 if (n < 0)
 {
 _putchar(45);
-putchar('\n');
+_putchar('\n');
 return (-1);
 }
 }
