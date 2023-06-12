7382d8b283724bc32bc970910cd1eb32aee2413a
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-06-12 08:33:57 +0100

    task done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,19 @@
  */
 int print_sign(int n)
 {
-switch (n)
+if (n>0)
 {
-case '(n > 0)':
-    return (1);
-    break;
-
-case '(n = 0)':
-    return (1);
-    break;
-
-case '(n < 0)':
-    return (-1);
-    break;
-
-default:
-    break;
+_putchar('+');
+return (1);
+}
+if (n = 0)
+{
+_putchar('0');
+return (0);
+}
+if (n < 0)
+{
+_putchar('-1');
+return (-1);
 }
 }
