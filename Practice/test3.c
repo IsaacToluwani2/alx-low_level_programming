56e3e786e4b7cf6e850a2a375fa7fec0ae3e159b
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-07-05 05:58:50 +0100

    files added

--- Practice/test3.c
+++ Practice/test3.c
@@ -2,5 +2,21 @@
 
 int main ()
 {
+ char array_ch[7] = {'H', 'e', 'l', 'l', 'o', '!', '\0'};
+ int i;
+
+ for (i = 0; i < 7; i++)
+ {
+  printf("array_ch[%d] contains: %c\n", i, array_ch[i]);
+  // method 1
+
+  printf("Put all elements together(Method 1):\n");
+  for (i = 0; array_ch[i] != '0' && i < 7; i++)
+  {
+    printf('%c', array)
+  }
+  
+ }
+ 
   
 }
