c1718a33509f6f95cfcb1584d3d2344eea9f16e1
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-08-17 05:45:57 +0100

    header files created

--- Practice/test.c
+++ Practice/test.c
@@ -4,19 +4,8 @@
 
 int main () 
 {
-// int i;
-// char str1[] = {'A', 'n', 'o'};
-// char str2[15];
-// char str3[15];
-
-// strcpy(str2, str1);
-// printf("The content of st2 %s\n", str2);
-
-// for (i = 0; i < str2[1]; i++)
-// {
-// str3[i] = str2[i];
-// }
-// str3[i] = '\0';
-// printf ("The content of st3 %s\n", str3);
+int j = 5;
+// int *p = &j;
+printf ("the value of j is %d", * &j);
 return (0);
 }
