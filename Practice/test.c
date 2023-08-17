327f2f433e2900810a2cc4471c1ce737c535c361
Isaac Toluwani <toluwani.isaac2@gmail.com>
2023-08-16 21:50:36 -0700

    changes were made to test.c

--- Practice/test.c
+++ Practice/test.c
@@ -2,21 +2,20 @@
 #include <math.h>
 #include <string.h>
 
+//function
+int add (int *a, int *b)
+{
+*a = 56;	
+int sum = *a + *b;
+return sum;
+}
+
+//main function
 int main () 
 {
-// int i;
-// char str1[] = {'A', 'n', 'o'};
-// char str2[15];
-// char str3[15];
-
-// strcpy(str2, str1);
-// printf("The content of st2 %s\n", str2);
-
-// for (i = 0; i < str2[1]; i++)
-// {
-// str3[i] = str2[i];
-// }
-// str3[i] = '\0';
-// printf ("The content of st3 %s\n", str3);
+int x = 5;
+int y = 6;
+printf ("%d\n", add(&x, &y));
+printf ("%d\n", x +  y);
 return (0);
 }
