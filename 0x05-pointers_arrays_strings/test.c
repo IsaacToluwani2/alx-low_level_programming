9e2f060fb6fcef5245e3be43e086cf737b2a0da0
IsaacToluwani2 <toluwani.isaac2@gmail.com>
2023-08-23 10:37:54 +0100

    practice file created

--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-#include "main.h"
-
 
 // Function
-int _strlen(char *s)
-{
-return strlen(s);
+int _strlen(char *s) {
+    int i = 0;
+    while (s =! 0)
+    {
+        i++;
+        s++;
+    }
+    
+    return i;
 }
 
 // Main Function
- int main(void)
-{
+int main(void) {
     char *str;
     int len;
 
     str = "My first strlen!";
     len = _strlen(str);
     printf("%d\n", len);
-    return (0);
+    return 0;
 }
