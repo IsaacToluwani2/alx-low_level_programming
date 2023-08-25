74e2810f3f9f8160f67c3071616cc607cb453d8b
Isaac Toluwani <toluwani.isaac2@gmail.com>
2023-08-25 14:53:00 -0700

    error in header name corected,content cp

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,35 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+
+/*
+* Desc: header file that contains the prototype and  the definition
+*       for all the function and the type written in this project
+*/
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - the singly linked listss
+ * @str: string - (malloc'ed string)
+ * @len: length of stringss
+ * @next: pointer to the next node
+ *
+ * Description: singly linked list nodes structures
+ *              for the Holberton projects
+ */
+typedef struct list_s
+{
+        char *str;
+        unsigned int len;
+        struct list_s *next;
+} list_t;
+
+size_t list_len(const list_t *h);
+size_t print_list(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
+void free_list(list_t *head);
+
+
+#endif /* LISTS_H */
