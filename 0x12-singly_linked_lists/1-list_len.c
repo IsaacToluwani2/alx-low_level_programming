3772b7ed6d8b3c2f0b90610380797c9af1a8d6e9
Isaac Toluwani <toluwani.isaac2@gmail.com>
2023-08-24 21:32:40 -0700

    list len task completed

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - Find number of the elements in
+ *            the link list_t lists.
+ * @h: The link list_t lists.
+ * @elements: a counter for size_t (nodes)
+ *
+ * Return: The no of elements in h.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
